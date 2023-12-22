e66c404772df6ebb0ca734488024770828378e7a
Alan Courtois <alan@wesur.fr>
2023-12-22 14:59:44 +0100

    Refactor email validation in authValidator middleware

--- src/Middlewares/authValidator.middleware.ts
+++ src/Middlewares/authValidator.middleware.ts
@@ -3,18 +3,18 @@ import prisma from "../utils/database";
 const signupValidator = [
     body("firstname").isString(),
     body("lastname").isString(),
-    body("email").isEmail().custom((value, { req }) => {
-        return prisma.user.findUnique({
+    body("email").isEmail().custom(async (value) => {
+        const user = await prisma.user.findUnique({
             where: {
                 email: value,
             },
-        }).then((user) => {
-            if (user) {
-                return Promise.reject("Email already in use");
-            }
-
-            return true;
         });
+
+        if (user) {
+            return Promise.reject("Email already exists");
+        }
+
+        return true;
     }),
     body("password").isLength({ min: 5 }),
 ];
